c7f5d582298d7bd7a45c504c6ccb4759b178ab4c
SNW-t9q <3280588715@qq.com>
2025-10-02 18:20:45 +0800

    完成了欢迎页面UI、图标、app名字的设计

---
+++ app/src/main/java/com/example/tripapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tripapp;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowInsetsController;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
